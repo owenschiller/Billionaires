134463ebf155c40a761c140edf163cbb42b70aba
owenschiller <owenschiller14@gmail.com>
2024-11-01 12:47:09 -0700

    changes

--- webapp.py
+++ webapp.py
@@ -21,6 +21,11 @@ def render_graph():
     names = get_names()
     return render_template('graph.html', names = names)
     
+@app.route("/graph2")
+def render_graph2():
+    names = get_names()
+    return render_template('graph2.html', names = names)
+    
 def get_names():
     with open('billionaires.json') as billionaires_data:
         info = json.load(billionaires_data)
@@ -29,14 +34,32 @@ def get_names():
     
 @app.route("/response")
 def render_response():
+
     with open('billionaires.json') as billionaires_data:
         info = json.load(billionaires_data)
     choice = request.args["billionaire"]
-    
+    count = 0
+    names = []
+    highest = []
+    years = []
     for i in info:
-        if i['name'] == choice:
-            response1 = choice + " is rank " + str(i['rank']) + " in the list of billioniares from 1994 to 2014"
-            response2 = choice + " is rank " + str(i['rank']) + " in the list of billioniares from 1994 to 2014"
+        if i["name"] not in names:
+            names.append(i["name"])
+            highest.append(i["rank"])
+            years.append(i["year"])
+        else:
+            x = names.index(i["name"])
+            if highest[x] < i["rank"]:
+                highest[x] = i["rank"]
+                years[x] = i["year"]
+    
+    if choice not in names:
+        return render_template('rank.html', response = "Please enter a valid name")
+        
+    for n in names:
+        if n == choice:
+            response1 = choice + " was highest ranked  at rank " + str(highest[names.index(n)]) + " in the year " + str(years[names.index(n)]) + "."
+    
         
 
 
@@ -53,16 +76,22 @@ def render_response1():
     for i in info:
         if i['name'] == choice:
             print((i['company']['founded']))
-            reply2 = choice + " is rank " + str(i['rank']) + " in the list of billioniares from 1994 to 2014. Their data was collected in " + str(i['year']) + "."
+            emerging = ""
             reply3 = "This wealth came from the company " + i['company']['name'] + ", which was founded in " + str(i['company']['founded']) + "."
+            
+            
+            if i['wealth']['how']['from emerging'] == True:
+                emerging = ", is from an emerging market, meaning that it's coming from a fast growing economy"
+            else: emerging = ", is not from an emerging market."
+            reply2 = choice + " 's company, " + i['company']['name'] + emerging + "."
             relationship = ""
             if i['company']['relationship'] == 'relation' or i['company']['relationship'] == '': 
                 relationship = ' an important member'
             else: relationship = "the " + i['company']['relationship']
             age = ""
             if i['demographics']['age'] == 0: 
-                age = 'has an unknown age' #str to int issue
-            else: age = "is " + str(i['demographics']['age']) + " years old" #str to int issue
+                age = 'has an unknown age'
+            else: age = "is " + str(i['demographics']['age']) + " years old" 
             
             if i['demographics']['gender'] == 'male':
                 reply4 = "He " + age + " and is " + relationship + " of the company."
